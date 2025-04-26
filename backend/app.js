0672dfb0d0ec5f6fd8d0877e056ff9f04a40741e
Prince Kumar <princekumarkha2005@gmail.com>
2025-04-26 14:54:10 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app=express();
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+}
+)
+
+//creating port
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+}
+)
